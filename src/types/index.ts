7a9414514df748f25aa64bd418767b71bd49665b
Alexander Markov <sasha.markov.mail@gmail.com>
2025-10-23 16:35:11 +0300

    Analyzed project starter. Developed Arhitecture. Updated Read.me

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,7 @@ export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+
+export interface Product {
+    
+}
